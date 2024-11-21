c4a411feccac27f4a68f2f8f0c4c9f9bc52b858d
Carlo Elvisi <carloelvisi01@outlook.it>
2024-11-21 17:06:31 +0100

    show funziona con ricerca id

--- app.js
+++ app.js
@@ -14,39 +14,6 @@ app.listen(port, () => {
   console.log(`In ascolto su port  ${port}`);
 });
 
-// array di ogetti
-const posts = [
-  {
-    titolo: "Ciambellone",
-    contenuto: "ciambellone",
-    immagine: "ciambellone.jpeg",
-    tags: ["Ciambellone", "ciambella", "grande"],
-  },
-  {
-    titolo: "Cracker Barbabietola",
-    contenuto: "Cracker Barbabietola",
-    immagine: "cracker_barbabietola.jpeg",
-    tags: ["cracker", "barbabietola"],
-  },
-  {
-    titolo: "Pane Fritto Dolce",
-    contenuto: "Pane Fritto Dolce",
-    immagine: "pane_fritto_dolce.jpeg",
-    tags: ["pane", "fritto", "dolce"],
-  },
-  {
-    titolo: "Pasta Barbabietola",
-    contenuto: "Pasta Barbabietola",
-    immagine: "pasta_barbabietola.jpeg",
-    tags: ["pasta", "barbabietola"],
-  },
-  {
-    titolo: "Torta Paesana",
-    contenuto: "Torta Paesana",
-    immagine: "torta_paesana.jpeg",
-    tags: ["torta", "paesana"],
-  },
-];
 // rotta /bacheca
 app.get("/bacheca", (req, res) => {
   const response = {
--- controllers/postsControllers.js
+++ controllers/postsControllers.js
@@ -1,12 +1,23 @@
+postsData = require("../data/posts");
+
 // Index - Recupera la lista di tutti i post
+
 function index(req, res) {
-  res.json("Lista dei post");
+  res.json(postsData);
 }
 
 // Show - Recupera i dettagli di un post specifico per ID
 function show(req, res) {
-  res.json("Dettagli del post con ID: " + req.params.id);
+  const id = parseInt(req.params.id);
+  const post = postsData.find((post) => post.id === id);
+
+  if (post) {
+    res.json(post);
+  } else {
+    res.status(404).json({ message: "Post not found" });
+  }
 }
+
 // Store - Crea un nuovo post
 function store(req, res) {
   res.json("Creazione di un nuovo post");
@@ -17,7 +28,20 @@ function update(req, res) {
 }
 // Delete - Elimina un post tramite ID
 function destroy(req, res) {
-  res.json("Eliminazione del post con ID: " + req.params.id);
+  const id = parseInt(req.params.id);
+  const postIndex = postsData.findIndex((post) => post.id === id);
+
+  if (postIndex === -1) {
+    // Se il post con il tag fornito non viene trovato
+    return res.status(404).json({ error: "Non trovato" });
+  }
+
+  // Rimuovi il post da postsData
+  postsData.splice(postIndex, 1);
+
+  console.log(postsData);
+
+  res.sendStatus(204);
 }
 
 module.exports = { index, show, store, update, destroy };
